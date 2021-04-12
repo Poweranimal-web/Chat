aefdaf3d8b1fcde23a0b59e16292749d56c8274e
Poweranimal-web <nikita.guseff@gmail.com>
2021-04-12 15:01:58 +0300

    Заменил в клиенте переменную data2 = ','.join([f'{key},{value}'for key,value in data_auth.items()]) на data2 = json.dumps(). На сервере с помoщью Json преобразовал строку  в dict.

--- srv.py
+++ srv.py
@@ -2,19 +2,24 @@ import json
 from twisted.internet import reactor, protocol
 from twisted.internet.protocol import Factory, Protocol
 port = 9090
+f = open('user.txt', 'w+')
 class Chat(Protocol):
     def __init__(self, factory, addr):
         self.factory = factory
-        self.factory.addr = addr
+        self.factory.addr = str(addr)
     def connectionMade(self):
        self.factory.numProtocols =+1
        self.transport.write("Welcome! There are currently %d open connections" .encode('utf-8')%(self.factory.numProtocols))
     def dataReceived(self, data:str):
         self.factory.ips.append(self.factory.addr)
-        # with open("host2.json", 'w+')as f:
-        # struc = {}
+        print(type(self.factory.addr))
+        # str1 = json.dumps(self.factory.addr)
         d = data.decode('utf-8')
         struc = json.loads(d)
+        struc['addres'] = self.factory.addr
+        str = json.dumps(struc)
+        f.write(str)
+        f.close()
         print('From Client:', struc)
         self.transport.write('Server  Answer.The data was received from client '.encode('utf-8'))
         # a = str(data)
