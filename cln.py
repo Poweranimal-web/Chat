8c374d9e4318aa1884b5f6eb103e0616e686b503
Poweranimal-web <nikita.guseff@gmail.com>
2021-04-13 20:48:03 +0300

    Неудачна попытка сделать регистрацию клиента

--- cln.py
+++ cln.py
@@ -1,42 +1,50 @@
 import json
 from twisted.internet import reactor, protocol
 from twisted.internet.protocol import ClientFactory, Protocol
+from twisted.protocols.basic import LineReceiver
 port = 9090
 check = input('Do you registred, already?')
 class ClientChat(Protocol):
      def connectionMade(self):
          print('connected')
-     def dataReceived1(self, data):
-         print(data)
-         login = input('Enter your  login: ')
-         password = input('Enter your password: ')
-         data_auth = {}
-         data_auth['login'] = login
-         data_auth['password'] = password
-         data2 = json.dumps(data_auth)
-         a = data2.encode('utf-8')
-         self.transport.write(a)
-     def dataReceived2(self, data):
-         print('pls authoristing')
-         login = input('Enter your  login: ')
-         password = input('Enter your password: ')
-         data_auth = {}
-         data_auth['login'] = login
-         data_auth['password'] = password
-         data2 = json.dumps(data_auth)
-         a = data2.encode('utf-8')
-         self.transport.write(a)
      def dataReceived(self, data):
          global check
          print(data)
          if check == 'no':
-            check1 = check.encode('utf-8')
-            self.transport.write(check1)
-            self.dataReceived1(data=bytes)
-         if check == 'yes':
              check1 = check.encode('utf-8')
              self.transport.write(check1)
-             self.dataReceived2(data=bytes)
+         elif check == 'yes':
+             check1 = check.encode('utf-8')
+             self.transport.write(check1)
+class ClientChat2(ClientChat):
+     def dataReceived(self, data):
+         if check == 'no':
+             print(data)
+             login = input('Enter your  login: ')
+             password = input('Enter your password: ')
+             data_auth = {}
+             data_auth['login'] = login
+             data_auth['password'] = password
+             data2 = json.dumps(data_auth)
+             a = data2.encode('utf-8')
+             self.transport.write(a)
+         elif check == 'yes':
+            print(data)
+            print('pls authoristing')
+            login = input('Enter your login: ')
+            password = input('Enter your password: ')
+            data_auth = {}
+            data_auth['login'] = login
+            data_auth['password'] = password
+            data2 = json.dumps(data_auth)
+            a = data2.encode('utf-8')
+            self.transport.write(a)
+
+
+
+
+
+
      def connectionLost(self, reason):
          print('disconnected, reason:', reason)
 class ClientChatFactory(ClientFactory):
--- srv.py
+++ srv.py
@@ -1,6 +1,7 @@
 import json
 from twisted.internet import reactor, protocol
 from twisted.internet.protocol import Factory, Protocol
+from twisted.protocols.basic import LineReceiver
 port = 9090
 f = open('user.txt', 'w+')
 class Chat(Protocol):
@@ -9,34 +10,32 @@ class Chat(Protocol):
         self.factory.addr = str(addr)
     def connectionMade(self):
        self.factory.numProtocols =+1
-       self.transport.write("Welcome! There are currently %d open connections" .encode('utf-8')%(self.factory.numProtocols))
-       self.dataReceived
-    def dataReceveid1(self, data):
-        d = data.decode('utf-8')
-        struc = json.loads(d)
-        struc['addres'] = self.factory.addr
-        str = json.dumps(struc)
-        if str in f:
-            self.transport.write('You are authorithed.Welcome %' % struc['login'].encode('utf-8'))
-        else:
-            self.transport.write('Wrong data, pls try again'.encode('utf-8'))
-    def dataReceveid2(self, data):
-        d = data.decode('utf-8')
-        struc = json.loads(d)
-        struc['addres'] = self.factory.addr
-        str = json.dumps(struc)
-        f.write(str)
-        f.close()
-        self.transport.write('Welcome % , you are registred' % struc['login'].encode('utf-8'))
+       self.transport.write('Hello from server'.encode('utf-8'))
     def dataReceived(self, data):
-        self.factory.ips.append(self.factory.addr)
-        b = data.decode('utf-8')
-        self.transport.write('data get'.encode('utf-8'))
-        if b == 'yes':
-             self.dataReceveid1(data=bytes)
-        elif b == 'no':
-             self.dataReceveid2(data=bytes)
-
+        self.a = (data).decode('utf-8')
+        if self.a == 'yes':
+             return Chat2
+        elif self.a == 'no':
+             return Chat2()
+class Chat2(Chat):
+    def dataReceived(self, data):
+         if self.a == 'no':
+            d = data.decode('utf-8')
+            struc = json.loads(d)
+            struc['addres'] = self.factory.addr
+            str = json.dumps(struc)
+            if str in f:
+                self.transport.write('You are authorithed.Welcome %' % struc['login'].encode('utf-8'))
+            else:
+                self.transport.write('Wrong data, pls try again'.encode('utf-8'))
+         elif self.a == 'yes':
+            d = data.decode('utf-8')
+            struc = json.loads(d)
+            struc['addres'] = self.factory.addr
+            str = json.dumps(struc)
+            f.write(str)
+            f.close()
+            self.transport.write('Welcome % , you are registred' % struc['login'].encode('utf-8'))
     def connectionLost(self, reason):
          print('dissconeted, reason:', reason)
          self.factory.numProtocols =- 1
