b0c2d7afbfea1d23bfbd140689b2b44cf17db35b
Poweranimal-web <nikita.guseff@gmail.com>
2021-06-22 16:29:54 +0300

    Добавил предачу файла между двумя клиентами,но еще надо добавить чтобы клиенту было видно то что ему отправили файл в чате.

--- srv2.py
+++ srv2.py
@@ -1,3 +1,5 @@
+import builtins
+import os
 import json
 from twisted.internet import reactor, protocol
 from twisted.internet.protocol import Factory, Protocol
@@ -11,6 +13,7 @@ from Check_data3 import query_with_fetchall
 from mysql.connector import MySQLConnection, Error
 from python_mysql_dbconfig import read_db_config
 port = 9090
+information_file = {}
 class Chat(Protocol):
     def __init__(self, factory, addr):
         self.factory = factory
@@ -22,96 +25,145 @@ class Chat(Protocol):
        a = json.dumps(hi)
        self.transport.write(a.encode('utf-8'))
     def dataReceived(self, data):
-        d = data.decode('utf-8')
-        struc = json.loads(d)
-        if struc['set'] == 'auth':
-            del struc['set']
-            login = struc['login']
-            password = struc['password']
-            a = query_with_fetchone1(login, password)
-            auth = {}
-            auth['set'] = 'auth'
-            c = json.dumps(auth)
-            no_auth = {}
-            no_auth['set'] = 'no auth'
-            g = json.dumps(no_auth)
-            if a == True:
-                self.transport.write(c.encode('utf-8'))
-            else:
-                self.transport.write(g.encode('utf-8'))
-        elif struc['set'] == 'registr':
-            del struc['set']
-            login = struc['login']
-            password = struc['password']
-            insert_book(login, password)
-            auth = {}
-            auth['set'] = 'auth'
-            k = json.dumps(auth)
-            self.transport.write(k.encode('utf-8'))
-        elif struc['set'] == 'write message':
-            del struc['set']
-            message = "{}:{}".format(struc['sender'], struc['message'])
-            c = struc['receipient']
-            insert_book1(c, message)
-            bring = {}
-            bring['set'] = 'bring'
-            a = json.dumps(bring)
-            self.transport.write(a.encode('utf-8'))
-        elif struc['set'] == 'Get':
-            del struc['set']
-            login = struc['nick']
-            q = query_with_fetchall(login)
-            delete_book(login)
-            if q['set'] == 'no':
-                no_mesg = {}
-                no_mesg['set'] = 'no mesg'
-                a = json.dumps(no_mesg)
-                self.transport.write(a.encode('utf-8'))
-            else:
-                cr = json.dumps(q)
-                self.transport.write(cr.encode('utf-8'))
-        # elif struc['set'] == 'GET USERS':
-        #     try:
-        #         dbconfig = read_db_config()
-        #         conn = MySQLConnection(**dbconfig)
-        #         cursor = conn.cursor()
-        #         cursor.execute("SELECT login FROM users")
-        #         rows = cursor.fetchall()
-        #         users = []
-        #
-        #         for row in rows:
-        #             users.append(row)
-        #
-        #
-        #     except Error as e:
-        #         print(e)
-        #
-        #     finally:
-        #         users2 = {}
-        #         users2['set'] = 'send users'
-        #         users2['users'] = users
-        #         dec = json.dumps(users2)
-        #         enc = dec.encode('utf-8')
-        #         self.transport.write(enc)
-        #         cursor.close()
-        #         conn.close()
-        elif struc['set'] == 'find friend':
-            answer = {}
-            answer2 = {}
-            del struc['set']
-            c = struc['user']
-            b = find_friend(c)
-            answer['set'] = 'OK'
-            answer2['set'] = 'NO'
-            g = json.dumps(answer)
-            h = json.dumps(answer2)
-            if b == True:
-                self.transport.write(g.encode('utf-8'))
-            else:
-                self.transport.write(h.encode('utf-8'))
+        global infromation_file
+        try:
+            d = data.decode('utf-8')
+            struc = json.loads(d)
+            print(struc)
+        except builtins.UnicodeDecodeError:
+                Chat2.dataReceived(self, data=data)
+        else:
+            if struc['set'] == 'auth':
+                        del struc['set']
+                        login = struc['login']
+                        password = struc['password']
+                        a = query_with_fetchone1(login, password)
+                        auth = {}
+                        auth['set'] = 'auth'
+                        c = json.dumps(auth)
+                        no_auth = {}
+                        no_auth['set'] = 'no auth'
+                        g = json.dumps(no_auth)
+                        if a == True:
+                            self.transport.write(c.encode('utf-8'))
+                        else:
+                            self.transport.write(g.encode('utf-8'))
+            elif struc['set'] == 'registr':
+                        del struc['set']
+                        login = struc['login']
+                        password = struc['password']
+                        insert_book(login, password)
+                        auth = {}
+                        auth['set'] = 'auth'
+                        k = json.dumps(auth)
+                        self.transport.write(k.encode('utf-8'))
+            elif struc['set'] == 'write message':
+                        del struc['set']
+                        message = "{}:{}".format(struc['sender'], struc['message'])
+                        c = struc['receipient']
+                        insert_book1(c, message)
+                        bring = {}
+                        bring['set'] = 'bring'
+                        a = json.dumps(bring)
+                        self.transport.write(a.encode('utf-8'))
+            elif struc['set'] == 'Get':
+                        del struc['set']
+                        login = struc['nick']
+                        q = query_with_fetchall(login)
+                        delete_book(login)
+                        if q['set'] == 'no':
+                            no_mesg = {}
+                            no_mesg['set'] = 'no mesg'
+                            a = json.dumps(no_mesg)
+                            self.transport.write(a.encode('utf-8'))
+                        else:
+                            cr = json.dumps(q)
+                            self.transport.write(cr.encode('utf-8'))
+            elif struc['set'] == 'find friend':
+                        answer = {}
+                        answer2 = {}
+                        del struc['set']
+                        c = struc['user']
+                        b = find_friend(c)
+                        answer['set'] = 'OK'
+                        answer2['set'] = 'NO'
+                        g = json.dumps(answer)
+                        h = json.dumps(answer2)
+                        if b == True:
+                            self.transport.write(g.encode('utf-8'))
+                        else:
+                            self.transport.write(h.encode('utf-8'))
+            elif struc['set'] == 'bring file':
+                        os.mkdir("C:/Users/millioner/PycharmProjects/Chat/Files/%s" % struc['receipient'])
+                        del struc['set']
+                        print('Received')
+                        with open("C:/Users/millioner/PycharmProjects/Chat/Files/%s/%s" % (struc['receipient'], struc['filename']), 'w') as f:
+                            f.write(struc['datafile'])
+            elif struc['set'] == 'bring file2':
+                        del struc['set']
+                        information_file['receipient'] = struc['receipient']
+                        information_file['filename'] = struc['filename']
+                        information_file['file_extension'] = struc['file_extension']
+                        Chat2.dataReceived(self,data=b'OK')
+            elif struc['set'] == 'Check File':
+                        del struc['set']
+                        if os.path.exists("C:/Users/millioner/PycharmProjects/Chat/Files/%s" % struc['dir']) and os.path.isdir("C:/Users/millioner/PycharmProjects/Chat/Files/%s" %struc['dir']):
+                             if len(os.listdir('C:/Users/millioner/PycharmProjects/Chat/Files/%s' % struc['dir'])) > 0:
+                                a = len(os.listdir('C:/Users/millioner/PycharmProjects/Chat/Files/%s'% struc['dir'])) * 2
+                                input_dir = r'C:\Users\millioner\PycharmProjects\Chat\Files\%s' % struc['dir']
+                                c = {}
+                                for root, dirs, filenames in os.walk(input_dir):
+                                     for filename in filenames:
+                                            # with open("C:/Users/millioner/PycharmProjects/Chat/Files/%s/%s" % (struc['dir'] , filename), 'rb') as f:
+                                            #     # enlargement = os.path.splitext(base)[1]
+                                            #     dataf = f.read()
+                                            c['filename'] = filename
+                                            c['set'] = 'get'
+                                            string = json.dumps(c)
+                                            self.transport.write(string.encode('utf-8'))
+                                            break
+                             else:
+                                 print('no file')
+                                 no_file = {}
+                                 no_file['set'] = 'no file'
+                                 self.transport.write((json.dumps(no_file)).encode('utf-8'))
+                        else:
+                            print('no exist')
+                            no_file = {}
+                            no_file['set'] = 'no exist'
+                            self.transport.write((json.dumps(no_file)).encode('utf-8'))
+            elif struc['set'] == 'transport file':
+                del struc['set']
+                input_dir = r'C:\Users\millioner\PycharmProjects\Chat\Files\%s' % struc['dir']
+                for root, dirs, filenames in os.walk(input_dir):
+                    for filename in filenames:
+                        with open("C:/Users/millioner/PycharmProjects/Chat/Files/%s/%s" % (struc['dir'] , filename), 'rb') as f:
+                            # enlargement = os.path.splitext(base)[1]
+                            dataf = f.read()
+                            self.transport.write(dataf)
+                            f.close()
+                            os.remove("C:/Users/millioner/PycharmProjects/Chat/Files/%s/%s" % (struc['dir'], filename))
+                            break
+
     def connectionLost(self, reason):
          print('dissconeted, reason:', reason)
          self.factory.numProtocols =- 1
+class Chat2(Protocol):
+    def dataReceived(self, data):
+        if data == b'OK':
+            r = {}
+            r['set'] = 'get'
+            v = json.dumps(r)
+            m = v.encode('utf-8')
+            self.transport.write(m)
+        else:
+            if os.path.exists("C:/Users/millioner/PycharmProjects/Chat/Files/%s" %(information_file['receipient'])) and os.path.isdir("C:/Users/millioner/PycharmProjects/Chat/Files/%s" % (information_file['receipient'])):
+                with open("C:/Users/millioner/PycharmProjects/Chat/Files/%s/%s" % (information_file['receipient'],information_file['filename']), 'wb') as f:
+                    f.write(data)
+            else:
+                os.mkdir("C:/Users/millioner/PycharmProjects/Chat/Files/%s" % (information_file['receipient']))
+                with open("C:/Users/millioner/PycharmProjects/Chat/Files/%s/%s" % (information_file['receipient'],information_file['filename']), 'wb') as f:
+                    f.write(data)
 class ChatFactory(Factory):
     def __init__(self):
         self.client = []
